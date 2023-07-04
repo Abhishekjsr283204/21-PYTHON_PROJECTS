89f12a4af81c4f40194f2e6dbf4cdf2264dc39a8
Abhishekjsr283204 <108180708+Abhishekjsr283204@users.noreply.github.com>
2023-07-04 12:49:11 +0600

    Update calendar maker.py

--- calendar maker.py
+++ calendar maker.py
@@ -3,7 +3,7 @@ import datetime
 DAYS = ('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday')
 MONTHS = ('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December')
 
-print('Calendar Maker, by Al Sweigart al@inventwithpython.com')
+print('Calendar Maker, by Abhishek kumar')
 
 while True:
     print('Enter the year for the calendar:')
