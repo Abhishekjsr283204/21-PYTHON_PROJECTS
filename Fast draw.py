89e71fad5e62596f7719c633805c06bfb5d8689b
Abhishekjsr283204 <108180708+Abhishekjsr283204@users.noreply.github.com>
2023-07-10 11:30:55 +0600

    Update Fast draw.py

--- Fast draw.py
+++ Fast draw.py
@@ -1,7 +1,7 @@
 import random, sys, time
 
-print('Fast Draw, by Al Sweigart al@inventwithpython.com')
-print()
+print('Fast Draw, by Abhishek')
+print()#blank line
 print('Time to test your reflexes and see if you are the fastest')
 print('draw in the west!')
 print('When you see "DRAW", you have 0.3 seconds to press Enter.')
